2f75e47011954dab9dea643ebfa7e2a8bc18e307
Antoine Albertelli <antoine@antoinealb.net>
2017-03-12 18:22:03 +0100

    Add sound capture script
    This fairly basic script takes data from an UART and output it to a WAV
    file.

---
+++ capture_sound.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Reads sounds from the microphone
+"""
+
+import argparse
+import progressbar
+import serial
+import wave
+import struct
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("port", help="Serial port")
+    parser.add_argument("output", help="WAV file for output")
+    parser.add_argument("--length", "-l", help="Buffer length (44.1k by default", type=int, default=44100)
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    conn = serial.Serial(args.port, 921600)
+
+    conn.write("dfsdm\r\n".encode())
+
+    buf = bytes()
+
+    print("Acquiring...")
+    while not buf.decode().endswith("Done !\r\n"):
+        buf = buf + conn.read(1)
+
+    print("done")
+
+    buf = bytes()
+    pbar = progressbar.ProgressBar(maxval=args.length).start()
+
+    while len(buf) < 4 * args.length:
+        pbar.update(len(buf) / 4)
+        buf += conn.read(100)
+
+    pbar.finish()
+
+    with wave.open(args.output, 'wb') as f:
+        f.setnchannels(1)
+        f.setsampwidth(4)
+        f.setframerate(44.1e3)
+        f.writeframes(buf)
+
+if __name__ == '__main__':
+    main()
